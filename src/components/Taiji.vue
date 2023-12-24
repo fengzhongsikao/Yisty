7edaad9fb6cfacfe0d083a917ca9c044ede2c5f8
five <five@QIANFENG>
2023-12-24 16:35:05 +0800

    添加速度控制

--- src/components/Taiji.vue
+++ src/components/Taiji.vue
@@ -1,28 +1,33 @@
 <script setup>
-import { ref} from 'vue'
+import {ref} from 'vue'
 
-const seconds=ref(2)
+const seconds = ref(2)
 const isAnimationRunning = ref(true);
-function swichStatus(){
-  isAnimationRunning.value=!isAnimationRunning.value
+
+function swichStatus() {
+  isAnimationRunning.value = !isAnimationRunning.value
 }
 </script>
 
 <template>
-    <div id="wrapper">
-        <img  ref="div" id="image" src="@/assets/yi.png" alt="1" @click="swichStatus" :style="{animationPlayState:`${isAnimationRunning==true?'running':'paused'}`}">
-    </div>
+  <div id="wrapper" class="container">
+
+    <img ref="div" id="image" src="@/assets/yi.png" alt="1" @click="swichStatus"
+         :style="{animationPlayState:`${isAnimationRunning==true?'running':'paused'}`,animationDuration:`${seconds}s`}">
+    <div class="pt-5" v-if="isAnimationRunning"><input  type="text" class="form-control rounded-0 " placeholder="输入一圈的秒速(为数字)" v-model="seconds"></div>
+  </div>
+
 </template>
 
 <style scoped>
-#image{
+#image {
   transform-origin: center;
   animation: rotate 2s linear infinite;
   width: 300px;
   height: 280px;
 }
 
-@keyframes rotate{
+@keyframes rotate {
   from {
     transform: rotate(0deg);
   }
